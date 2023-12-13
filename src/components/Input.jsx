4cd20196f0084da3e3b7132e3bb627e079d6b59a
Elena Korchuganova <korchel21@gmail.com>
2023-12-13 22:30:43 +0300

    fieldarray added

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useSelector } from 'react-redux';
-import { Form, Formik, Field } from 'formik';
+import { Formik, Form, Field, FieldArray } from 'formik';
 
 import Result from "./Result";
 
@@ -17,76 +17,83 @@ const Input = () => {
           upsEfficiency: 0,
           dischargeDepth: 0,
           availableCapacity: 0,
+          loads: [{id: 1, value: 15}, {id: 2, value: 25}],
           totalLoad: 0,
         }}
-        onSubmit={() => {
-          
+        onSubmit={(values) => {
+          console.log(values)
         }}
       >
-        <Form>
-          <h2 className='title'>Исходные данные:</h2>
-          <div className='input-group'>
-            <label className='input-label'>U - Напряжение, выдаваемое задействованной АКБ, В</label>
-            <Field
-              name='voltage'
-              className='ups-input'
-              type='number'
-            />
-          </div>
-          <div className='input-group'>
-            <label className='input-label'>C - емкость задействованной АКБ, Ач</label>
-            <Field
-              name='capacitance'
-              className='ups-input'
-              type='number'
-            />
-          </div>
-          <div className='input-group'>
-            <label className='input-label'>КПД источника бесперебойного питания</label>
-            <Field
-              name='upsEfficiency'
-              className='ups-input'
-              type='number'
-            />
-          </div>
-          <div className='input-group'>
-            <label className='input-label'>K - глубина разряда батареи</label>
-            <Field
-              name='dischargeDepth'
-              className='ups-input'
-              type='number'
-            />
-          </div>
-          <div className='input-group'>
-            <label className='input-label'>Kg - доступная емкость</label>
-            <Field
-              name='availableCapacity'
-              className='ups-input'
-              type='number'
-            />
-          </div>
-          <div>
-            <h4>Нагрузка, Вт:</h4>
-            <button type='button' onClick={() => setLoadInputs(loadInputs + 1)}>+</button>
+        {({ errors, touched, values }) => (
+          <Form>
+            <h2 className='title'>Исходные данные:</h2>
             <div className='input-group'>
-              <label className='input-label'>{`прибор 1`}</label>
-              <div className="input-group">
-                <Field
-                  className='ups-input'
-                  type="number"
-                  name="totalLoad"
-                />
-                <button>-</button>
-              </div>
+              <label className='input-label'>U - Напряжение, выдаваемое задействованной АКБ, В</label>
+              <Field
+                name='voltage'
+                className='ups-input'
+                type='number'
+              />
             </div>
-          </div>
-          <button type='submit'>
-            Рассчитать
-          </button>
-        </Form>
-        <button onClick={() => setShowResult(!showResult)} > show </button>
+            <div className='input-group'>
+              <label className='input-label'>C - емкость задействованной АКБ, Ач</label>
+              <Field
+                name='capacitance'
+                className='ups-input'
+                type='number'
+              />
+            </div>
+            <div className='input-group'>
+              <label className='input-label'>КПД источника бесперебойного питания</label>
+              <Field
+                name='upsEfficiency'
+                className='ups-input'
+                type='number'
+              />
+            </div>
+            <div className='input-group'>
+              <label className='input-label'>K - глубина разряда батареи</label>
+              <Field
+                name='dischargeDepth'
+                className='ups-input'
+                type='number'
+              />
+            </div>
+            <div className='input-group'>
+              <label className='input-label'>Kg - доступная емкость</label>
+              <Field
+                name='availableCapacity'
+                className='ups-input'
+                type='number'
+              />
+            </div>
+            <div>
+              <h4>Нагрузка, Вт:</h4>
+              <button type='button' onClick={() => setLoadInputs(loadInputs + 1)}>+</button>
+              <FieldArray name="loads" render={({insert, remove}) => {
+                return values.loads.map((load, index) => (
+                  <div className='input-group' key={`load${index}`}>
+                    <label className='input-label'>{`прибор ${index + 1}`}</label>
+                    <div className="input-group">
+                      <Field
+                        id={`load${index}`}
+                        name={`load${index}`}
+                        className='ups-input'
+                        type="number"
+                      />
+                      <button>-</button>
+                    </div>
+                  </div>
+                ))
+              }} />
+            </div>
+            <button type='submit'>
+              Рассчитать
+            </button>
+          </Form>
+        )}
       </Formik>
-      
+      <button onClick={() => setShowResult(!showResult)} > show </button>
       {showResult && <Result />}
     </>
   );
