fca40fe6980a5753de60e71aaac76a9214bcdb80
Elena Korchuganova <korchel21@gmail.com>
2023-12-17 20:34:27 +0300

    exceljs added

---
+++ src/components/Table.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Excel from 'exceljs';
+import { saveAs } from 'file-saver';
+
+const Table = () => {
+  const workbook = new Excel.Workbook();
+  
+};
+
+export default Table;
